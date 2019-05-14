0a1cff0bc7e1f904a5595596b66b54817d065da6
ibhuiyan <ibhuiyan@umich.edu>
2019-05-14 15:00:46 -0400

    Request uses async/await. Initialized array to hold top songs.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 
 const express = require('express');
-const axios = require('axios');
 const SpotifyRequests = require('./spotify-requests');
 
 const auth = require('./auth/app');
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+// let topTracks = [];
 
 
 app.use((req, res, next) => {
@@ -17,9 +18,28 @@ app.use((req, res, next) => {
 
 // GET route
 app.get('/api/recents', (req, res) => {
-  const token = 'BQBABE87R_ourSyXtRPQRWFeAuSObQCtgTF6NsocwnZxa67dsvju5RiK8GApKal81TrMygShE0Ta6b8baCJA9U74TCZlKYm5KYKDpWGW4cIWJlNhAJCSGb9BGSXBsy8BjXPcymMQPJ6SXB9jdYDThzq5wF8SMHB-gDoe6U7D-wCFmzJNQQ';
+  const token = 'BQDBODXtceDr16hPSS6G9lwOgKi7wiGrsklNR_7etvwiSrQZYMr-G1L4lW3TeTJAP_jwp6SoNdXe4vMH0dGFe1mJToOKOQSiy8sskVVtm3rMDSTUE8rgGEitc8CkkMj9dzq9K9-ujHjdim9GOUsL4-YZhJp64-HOEryeeaOcHr3oY0I6Gg';
   const spotify = new SpotifyRequests(token);
-  spotify.getTopTracks().then(resp => console.log(resp));
+
+  spotify.getTopTracks()
+    .then((spotifyResponse) => {
+      const topTracks = spotifyResponse.map(trackObject => ({ // filter tracklist array
+        trackID: trackObject.id,
+        name: trackObject.name,
+        artists: trackObject.artists.map(artistObj => artistObj.name),
+      }));
+      res.send(topTracks);
+    });
+  /*
+  spotify.getTopTracks()
+    .then((data) => {
+      console.log(data.items);
+      // topTracks = [...data.items];
+      // console.log(topTracks);
+    })
+    .catch((reject) => {
+      console.log(reject);
+    }); */
 
   /*
   res.send(spotify.getTopTracks().then((data) => {
--- server/spotify-requests.js
+++ server/spotify-requests.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 
-module.exports = class SpotifyRequest {
+module.exports = class SpotifyRequests {
   // class representing reqests to the Spotify Web API
   constructor(accessToken) {
     this.token = accessToken;
@@ -16,6 +16,16 @@ module.exports = class SpotifyRequest {
     });
   }
 
+  async getTopTracks() {
+    // return a list of user's top 50 favorite songs
+    try {
+      const response = await this.instance.get('me/top/tracks?limit=50&time_range=short_term');
+      return response.data.items;
+    } catch (err) {
+      return err;
+    }
+  }
+  /*
   getTopTracks() {
     // Get user's top tracks.
     return this.instance.get('me/top/tracks')
@@ -26,5 +36,5 @@ module.exports = class SpotifyRequest {
         console.log(error);
         return Promise.reject(error);
       });
-  }
+  } */
 };
