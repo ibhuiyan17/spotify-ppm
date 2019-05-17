9cf1ab6bc206648a326bf7db7fc4c016af8dab36
ibhuiyan <ibhuiyan@umich.edu>
2019-05-17 17:39:20 -0400

    Now uses median for features instead of avg

--- server/server.js
+++ server/server.js
@@ -13,10 +13,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
-const token = 'BQB5OeDwLKU8XoqVgu-QzvvYe60Cv_nwDDK3cjxgrRij3M2e2kxJT3aKFwTuFAt0znbCIk5UgQh07ch0AtGFZeFEG3vFCaD78zVmsd4M9BeIsCMjFoN88LTEy-2h1yScYsjc6bNbN4YBYRMrzkpA3XScEb-6LT96Lx139-_hQcwSMoQ9EA';
+const token = 'BQCYhqdgQAj03Kbf_kFUb3La1iVuq_ka-8mBZvEk1gJeZ8b_FmjMU-IfU-Rvdklm4gKeiOvSoKzy-hK-H0zUJM6iKcn-wCx11J8mix_STA0SWXC63Hb4VNmMBwODvIuJVp5qtRwtdJB-LlKwcoDpw7tokSeEgWv6QoBxqGcSj3qsg79D-g';
 const spotify = new SpotifyRequests(token);
 let topTracks = []; // array of track objects containing trackID, name, and list of artists
 let averageFeatures = {};
+let medianFeatures = {};
 
 // GET route
 app.get('/api/top', async (req, res) => {
@@ -24,7 +25,8 @@ app.get('/api/top', async (req, res) => {
     // get top tracks and filter results
     topTracks = await utils.getFilteredTopTracks(spotify, req.query.time_range);
     averageFeatures = await utils.calculateAverageFeatures(spotify, topTracks);
-    res.send(averageFeatures);
+    medianFeatures = await utils.calculateMedianFeatures(spotify, topTracks);
+    res.send(medianFeatures);
   } catch (err) {
     console.log(err);
   }
--- server/utils.js
+++ server/utils.js
@@ -6,8 +6,69 @@
 */
 async function getFilteredTopTracks(spotifyInstance, timeRange) {
   try {
-    return $filterTrackList(await spotifyInstance.getTopTracks(timeRange));
+    return (await spotifyInstance.getTopTracks(timeRange)).map(trackObj => ({ // filter tracklist array
+      trackID: trackObj.id,
+      name: trackObj.name,
+      artists: trackObj.artists.map(artistObj => artistObj.name),
+    }));
   } catch (err) {
+    return err;
+  }
+}
+
+/*
+  input: spotify request object, array of track objects
+  output: object containing median features of input tracks
+*/
+async function calculateMedianFeatures(spotifyInstance, trackList) {
+  const medianFeatures = {}; // {feature -> median/list -> result}
+
+  // build set of features to exclude
+  const excludeSet = {};
+  [
+    'key', 'mode', 'type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature',
+  ].forEach((feature) => {
+    excludeSet[feature] = true;
+  });
+
+  try {
+    const featureList = await spotifyInstance.getFeatures(trackList.map(track => track.trackID));
+    const lenFeatureList = featureList.length;
+    featureList.forEach((featureObj) => {
+      Object.keys(featureObj).forEach((feature) => {
+        if (feature in excludeSet === false) { // only consider important features
+          if (feature in medianFeatures === false) { // initialize feature if not present
+            medianFeatures[feature] = {
+              median: 0,
+              list: [],
+            };
+          }
+          medianFeatures[feature].list.push(featureObj[feature]);
+        }
+      });
+    });
+    console.log(medianFeatures);
+
+    const type = (lenFeatureList % 2 === 0) ? 'even' : 'odd';
+    Object.keys(medianFeatures).forEach((feature) => {
+      console.log('feature list: ', medianFeatures[feature].list);
+      medianFeatures[feature].list.sort((a, b) => a - b);
+      switch (type) {
+        case 'even':
+          medianFeatures[feature].median = (medianFeatures[feature].list[(lenFeatureList / 2) - 1] + medianFeatures[feature].list[lenFeatureList / 2]) / 2;
+          break;
+        case 'odd':
+          medianFeatures[feature].median = feature.list[Math.floor(lenFeatureList / 2)];
+          break;
+        default:
+          break;
+      }
+    });
+    console.log('median features');
+    return medianFeatures;
+  } catch (err) {
+    console.log('err');
+    console.log(err);
     return err;
   }
 }
@@ -17,6 +78,7 @@ async function getFilteredTopTracks(spotifyInstance, timeRange) {
   output: object containing average features of input tracks
 */
 async function calculateAverageFeatures(spotifyInstance, trackList) {
+  // console.log('size: ', trackList.length, trackList);
   const averageFeatures = {};
 
   // build set of features to exclude
@@ -58,5 +120,6 @@ function $filterTrackList(trackList) {
 
 module.exports = {
   getFilteredTopTracks,
+  calculateMedianFeatures,
   calculateAverageFeatures,
 };
